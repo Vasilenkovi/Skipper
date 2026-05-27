ca6acc9621109d51f8983886c5faf45fa07b9ec3
psskllra-prog <psskllra@gmail.com>
2026-05-28 00:18:06 +0700

    Update TestApplication.kt

--- backend/src/test/kotlin/com/example/TestApplication.kt
+++ backend/src/test/kotlin/com/example/TestApplication.kt
@@ -2,25 +2,26 @@
 
 package com.example
 
-import com.example.features.competences.Competences
-import com.example.features.reviews.Reviews
-import com.example.features.slots.Slots
-import com.example.features.users.Users
-import io.ktor.server.testing.ApplicationTestBuilder
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
+import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.testApplication
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import com.example.features.competences.tables.Competences
+import com.example.features.reviews.tables.Reviews
+import com.example.features.slots.tables.Slots
+import com.example.features.users.tables.Users
 
-fun createTestDatabase(): Database =
-  Database.connect(
-    url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false;MODE=PostgreSQL",
-    driver = "org.h2.Driver",
-    user = "test",
-    password = "",
-  )
+fun createTestDatabase(): Database = Database.connect(
+  url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false;MODE=PostgreSQL",
+  driver = "org.h2.Driver",
+  user = "test",
+  password = "",
+)
 
-fun testApplicationWithDb(block: suspend ApplicationTestBuilder.() -> Unit) {
+fun testApplicationWithDb(block: suspend TestApplicationEngine.() -> Unit) {
   val db = createTestDatabase()
   transaction(db) {
     SchemaUtils.create(Users, Competences, Slots, Reviews)
