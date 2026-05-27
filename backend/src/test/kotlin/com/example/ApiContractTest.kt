082b5c7c205d6e1c96d2a5436d73890f02bf10f1
psskllra-prog <psskllra@gmail.com>
2026-05-28 00:59:50 +0700

    Update ApiContractTest.kt

--- backend/src/test/kotlin/com/example/ApiContractTest.kt
+++ backend/src/test/kotlin/com/example/ApiContractTest.kt
@@ -2,12 +2,12 @@
 
 package com.example
 
-import io.ktor.client.request.contentType
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.server.testing.testApplication
 import kotlin.test.Test
 import kotlin.test.assertNotNull
