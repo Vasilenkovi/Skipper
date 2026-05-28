598f022dd555e1214878708a165775656f070ec6
psskllra-prog <psskllra@gmail.com>
2026-05-28 18:01:08 +0700

    Update build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -37,6 +37,14 @@ detekt {
   autoCorrect = false
 }
 
+
+tasks.test {
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
+
 dependencies {
   implementation("io.ktor:ktor-server-status-pages")
   implementation("org.mindrot:jbcrypt:0.4")
@@ -52,11 +60,14 @@ dependencies {
   implementation(ktorLibs.server.core)
   implementation(ktorLibs.server.netty)
   implementation(libs.logback.classic)
-
-  testImplementation(kotlin("test"))
-  testImplementation(ktorLibs.server.testHost)
-  testImplementation("com.h2database:h2:2.2.224")
-
   implementation("io.ktor:ktor-server-auth-jvm")
   implementation("io.ktor:ktor-server-auth-jwt-jvm")
+
+  testImplementation(kotlin("test"))
+  testImplementation(ktorLibs.server.testHost) 
+  testImplementation("com.h2database:h2:2.2.224")
+
+  testImplementation("org.testcontainers:testcontainers:1.19.7")
+  testImplementation("org.testcontainers:postgresql:1.19.7")
+  testImplementation("org.testcontainers:junit-jupiter:1.19.7")
 }
