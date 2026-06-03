44e7db8e8b64f92678c6b551d613e6b5d5ad4951
konst <konstantiniglakov@gmail.com>
2026-06-03 19:08:05 +0700

    удалил ненужное

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,9 @@
-//import kotlin.io.path.name
-//
-//// N:/SkipperNew/settings.gradle.kts
-//rootProject.name = "SkipperNew"
-//
-//include(":mobile_change")
-//project(":mobile_change").projectDir = file("mobile_change")
-//
-//
-//rootProject.name = "SkipperNew"
-//
-//include(":mobile_change")project(":mobile_change").projectDir = file("mobile_change")
-//
-//// Если внутри mobile_change есть свой модуль app (как я видел ранее):
-//include(":mobile_change:app")
+
 
 rootProject.name = "SkipperNew"
 
-// Подключаем проект mobile_change (если в нем есть build.gradle.kts)
 include(":mobile_change")
 project(":mobile_change").projectDir = file("mobile_change")
 
-// Подключаем модуль app, который находится внутри mobile_change
 include(":app")
 project(":app").projectDir = file("mobile_change/app")
